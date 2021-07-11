7467b7cdcf1e72b3496d96bb7bcdbac21ccbf030
sahaynitin4tellyfun <87252200+sahaynitin4tellyfun@users.noreply.github.com>
2021-07-11 13:37:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,8 @@
 # Edit anything at your own risk!
 
 # Don't forget to help me if I done any mistake in the codes.
-# Support Group: @DevsZone 
-# Bots Channel: @Discovery_Updates
+# Support Group: @tellyfun4u 
+# Bots Channel: @tellyfun4u
 
 
 import os
