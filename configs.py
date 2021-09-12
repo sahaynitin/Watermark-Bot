128af0819b085141108019a48ba646a322ef76b9
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-09-12 17:47:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -18,7 +18,7 @@ class Config(object):
 	DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
 	PRESET = os.environ.get("PRESET", "ultrafast")
 	OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
-	CAPTION = "By @Tellybots"
+	CAPTION = "By @Tellybots_4u"
 	BOT_USERNAME = os.environ.get("BOT_USERNAME", "VideoWatermark_Bot")
 	DATABASE_URL = os.environ.get("DATABASE_URL")
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
@@ -29,7 +29,7 @@ Hi, I am Video Watermark Adder Bot!
 **How to Added Watermark to a Video?**
 **Usage:** First Send a JPG Image/Logo, then send any Video. Better add watermark to a MP4 or MKV Video.
 
-__Note: I can only process one video at a time. As my server is Heroku, my health is not good. If you have any issues with Adding Watermark to a Video, then please Report at [💬Support Group](https://t.me/tellybotz_support).__
+__Note: I can only process one video at a time. As my server is Heroku, my health is not good. If you have any issues with Adding Watermark to a Video, then please Report at [💬Support Group](https://t.me/tellybots_support).__
 
 """
 	PROGRESS = """
